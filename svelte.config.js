a40cfbec78afbbac24a234ebb6283276474e6b9c
Robert Bennett <rltbennett@gmail.com>
2025-09-07 23:40:32 -0400

    Update github pages action

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,8 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() },
+	kit: { adapter: adapter({ pages: "build", assets: "build", strict: true }) },
+	paths: { base: process.env.NODE_ENV === "production" ? "/collagen" : "" },
 };
 
 export default config;
